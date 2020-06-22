6ccfda2dff9adff76ecfc906b8b644d87af1cde4
Karson <nicholsk18@gmail.com>
2020-06-21 19:00:18 -0500

    added a simple window

--- main.cpp
+++ main.cpp
@@ -2,13 +2,38 @@
 #include <SDL2/SDL.h>
 
 int main() {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL init failed." << std::endl;
         return 1;
     }
 
-    std::cout << "SDL Init succeeded." << std::endl;
+    SDL_Window *window = SDL_CreateWindow("Particle Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+    if (window == nullptr) {
+        SDL_Quit();
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return 2;
+    }
+
+    // window open?
+    bool isRunning = false;
+
+    SDL_Event event;
+
+    while (!isRunning) {
+        while(SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT) {
+                isRunning = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
