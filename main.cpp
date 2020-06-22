5c3fb95c37162d86c7e19e2a24a606dda2bc7b76
Karson <nicholsk18@gmail.com>
2020-06-21 20:02:10 -0500

    created a blank white window

--- main.cpp
+++ main.cpp
@@ -20,6 +20,34 @@ int main() {
         return 2;
     }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    if (renderer == nullptr) {
+        std::cout << "Could not create renderer" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+
+    if (texture == nullptr) {
+        std::cout << "Could not create texture" << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 4;
+    }
+
+    // need to write error checking make sure it can allocate mem
+    Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+//
+//    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+//    SDL_RenderClear(renderer);
+//    SDL_RenderCopy(renderer, texture, NULL, NULL);
+//    SDL_RenderPresent(renderer);
+
     // window open?
     bool isRunning = false;
 
@@ -31,8 +59,19 @@ int main() {
                 isRunning = true;
             }
         }
+
+        SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
 
+    // free pointer
+    delete[] buffer;
+
+    // destroy sdl
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
